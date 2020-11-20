3d6def79eec02c9984952d7ad47e7175326e84e1
jeremy763 <xyh1997jeremy@gmail.com>
2020-11-19 19:18:46 -0700

    update compete

--- test.py
+++ test.py
@@ -2,53 +2,40 @@ from board import ReversiBoard
 from greedy import greedy
 import random
 
+def compete(board, player1, player2, gameNum, score):
+    while True:
+        board.showBoard()
+        if board.isEnd():
+            winner = board.getWinner()
+            
+            if winner == player1:
+                score["Player1"] += 1
+            elif winner == player2:
+                score["Player2"] += 1
+            break
+
+        else:
+            # player1 is random player
+            moves = board.getAllLegalMoves(player1)
+            board.play(player1, random.choice(moves))
+            # import player2 here
+            point = greedy(board, player2)
+            board.play(player2, point) 
+
+
 def main():
     board = ReversiBoard(8)
     player1 = "b"
     player2 = "w"
     gameNum = 10
-    count = 1
+    count = 0
     score = {"Player1": 0, "Player2": 0}
-    while count != 11:
-        while True:
-            board.showBoard()
-            if board.isEnd():
-                winner = board.getWinner()
-                
-                if winner == player1:
-                    score["Player1"] += 1
-                elif winner == player2:
-                    score["Player2"] += 1
-                count += 1
-                break
-            else:
-                # player1 is random player
-                moves = board.getAllLegalMoves(player1)
-                board.play(player1, random.choice(moves))
-                point = greedy(board, player2)
-                board.play(player2, point)
+    while count != 10:
+        compete(board, player1, player2, gameNum, score)
+        count += 1
     print("Total game played:", gameNum)
     print(score)
 
-    
-    '''
-        cmd = input('')
-        if len(cmd)==0:
-            print('\n ... adios :)\n')
-            return
-        if cmd[0][0]=='h':
-            
-            board.printMenu()
-        elif cmd[0][0]=='p':
-            
-            board.genMove(color)
-            if color == "b":
-                color = "w"
-            elif color == "w":
-                color = "b"
-        elif cmd =='b 19':
-            board.erase(19)
-            '''
 
 if __name__ == '__main__':
     main()
